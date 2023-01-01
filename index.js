69729c9017fa8e6d7e1709022886906db3a5438e
Ariba-memon <aribaabdulqadir786@gmail.com>
2023-01-01 15:44:27 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import figlet from "figlet";
+figlet.defaults({ fontPath: "assets/fonts" });
+function ready() {
+    console.log(figlet.textSync('Happy New Year 2023'));
+}
+ready();
+async function askQuestionone() {
+    let answers = await inquirer
+        .prompt([
+        /* Pass your questions in here */
+        {
+            type: "name",
+            name: "Name",
+            message: "Enter Your Name:"
+        },
+        {
+            type: "name",
+            name: "SecondName",
+            message: "Name of a Person You Want to Wish a Happy New Year:"
+        },
+    ]);
+    console.log(chalk.blue.bgRed.bold(`Welcome To New Year 2023 To my Friend ${answers.SecondName} Wishing you a very happy New Year! May your 2023 be filled with love & Success Ameen`));
+    console.log(chalk.hex('#DEADED').bold(`From  ${answers.Name}`));
+}
+async function startAgain() {
+    do {
+        await askQuestionone();
+        var again = await inquirer
+            .prompt({
+            type: "input",
+            name: "restart",
+            message: "Do you want to continue? Press y or n:"
+        });
+    } while (again.restart == 'y' || again.restart == 'Y' || again.restart == 'yes' || again.restart == 'YES');
+}
+startAgain();
